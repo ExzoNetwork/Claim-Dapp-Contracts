1ea5ee92ae7e74304d4e4925d096336310e5c91e
raebeatrose4 <rasbinbot@gmail.com>
2023-11-14 00:30:20 +0530

    ready for mainnet

--- scripts/deployMerkleDistributor.js
+++ scripts/deployMerkleDistributor.js
@@ -5,8 +5,8 @@ const { ethers } = require('hardhat')
 async function main() {
   const MerkleDistributor = await ethers.getContractFactory('MerkleDistributor')
   const merkleDistributor = await MerkleDistributor.deploy(
-    '0x58fa146077d723e7c65bDb16454589E03f3d914e', //TODO:REPLACE ADDRESS
-    '0x3d82ea98a48ce1c37258d7f8a1cb64df2e6dcd482c6d4d0dd12c88d7b156e925' //TODO:REPLACE ROOT HASH
+    '0xD098A30AE6C4A202DAD8155DC68e2494eBAc871c', //TODO:REPLACE ADDRESS
+    '0x98b0f46bc8a1f020766f49be102664751d0fcc698a88d2cf15bda8f1c536ebfb' //TODO:REPLACE ROOT HASH
   )
   await merkleDistributor.deployed()
   console.log(`merkleDistributor deployed at ${merkleDistributor.address}`)
